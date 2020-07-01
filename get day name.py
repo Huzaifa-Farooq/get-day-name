4b3f5e612333ded8c48319fd14620cdf8b3fa583
Huzaifa-Farooq <62528490+Huzaifa-Farooq@users.noreply.github.com>
2020-07-01 08:38:24 +0500

    Update get day name.py
    This code will return day name provided date in YYYY-MM-DD format.

--- get day name.py
+++ get day name.py
@@ -1,5 +1,4 @@
 import calendar
-from weekly_forecast import WeeklyWeatherForecast as wf
 
 
 def day_name(i):
@@ -12,21 +11,3 @@ def day_name(i):
     month = f"{date_list[4]}{date_list[5]}"
     day = f"{date_list[6]}{date_list[7]}"
     return days_list.pop(calendar.weekday(int(year), int(month), int(day)))
-
-def date_month(i):
-    """return date and month name"""
-    month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    date_list = list(wf(i).date())
-    month_index = int(f"{date_list[5]}{date_list[6]}") - 1
-    date = f"{date_list[8]}{date_list[9]}"
-    return f"{date} {month_names.pop(month_index)}"
-
-def date(i):
-    """returns only date"""
-    date_list = list(wf(i).date())
-    date = list(f"{date_list[8]}{date_list[9]}")
-    if int(date[0]) == 0:
-        return f"{date[1]}"
-    else:
-        return f"{date[0]}{date[1]}"
-print(date(4))
